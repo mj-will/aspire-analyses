df3dfa91cb6225a704ae9d6a62db8ee59c3bfa06
Michael J. Williams <michaeljw1@googlemail.com>
2025-11-04 13:48:37 +0000

    fix conversion

--- src/aspire_analysis_tools/gw/conversion.py
+++ src/aspire_analysis_tools/gw/conversion.py
@@ -7,7 +7,9 @@ def generate_all_bbh_parameters(sample, likelihood=None, priors=None, npool=1):
     output_sample = bilby_generate_all_bbh_parameters(sample, likelihood, priors, npool=npool)
     if "delta_phase" not in output_sample:
         output_sample["delta_phase"] = np.mod(
-            output_sample["phase"] + np.sign(np.cos(output_sample["theta_jn"]) * output_sample["psi"]),
+            output_sample["phase"]
+            + np.sign(np.cos(output_sample["theta_jn"]))
+            * output_sample["psi"],
             2 * np.pi,
         )
     return output_sample
